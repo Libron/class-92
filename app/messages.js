020cda6c347d853a59fe5230db74e62ec05f7da6
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-16 01:58:03 +0600

    Commit

--- app/messages.js
+++ app/messages.js
@@ -4,7 +4,12 @@ const Message = require('../models/Message');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const messages = await Message.find().populate('user');
+    let limit = null;
+
+    if (req.query.limit) {
+        limit = parseInt(req.query.limit);
+    }
+    const messages = await Message.find().sort('-datetime').limit(limit).populate('user');
     res.send(messages);
 });
 
